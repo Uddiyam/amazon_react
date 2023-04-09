12f283e103607b33195d5f6472606ddc4541a82d
Uddiyam <yjasd1234@naver.com>
2023-04-09 23:08:13 +0900

    최종 커밋

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
-
 import { Provider } from "react-redux";
 import store from "./components/store";
-
 import Router from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Router />
     </BrowserRouter>
   </Provider>
